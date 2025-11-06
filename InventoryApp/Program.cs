63ced31c619757b1daa2cc61bd5da09b61bb9e30
lambkath13 <avazuldasev70@gmail.com>
2025-11-07 01:21:44 +0500

    fix program

--- InventoryApp/Program.cs
+++ InventoryApp/Program.cs
@@ -120,6 +120,8 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<ApplicationDbContext>();
+    await dbContext.Database.MigrateAsync();
     await Seed.EnsureRolesAndAdminAsync(services);
 }
 
